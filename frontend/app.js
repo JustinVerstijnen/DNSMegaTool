887a27b92e47f2a423932c9cccc6a2b97c897232
Justin Verstijnen <info@justinverstijnen.nl>
2025-06-11 22:23:14 +0200

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -18,42 +18,40 @@ async function checkDomain() {
 
         const tooltips = {
             "MX": {
-                text: "Mail Exchange record, checks if a record is configured",
-                link: "https://mxtoolbox.com/technical/mx-records"
+                text: "Mail Exchange record, checks if a record is configured. ",
+                link: "https://justinverstijnen.nl/enhance-email-security-with-spf-dkim-dmarc/#mx"
             },
             "SPF": {
-                text: "Sender Policy Framework, checks if a record is configured and is using hardfail (-all)",
-                link: "https://dmarcian.com/spf/"
+                text: "Sender Policy Framework, checks if a record is configured and is using hardfail (-all). ",
+                link: "https://justinverstijnen.nl/enhance-email-security-with-spf-dkim-dmarc/#spf"
             },
             "DKIM": {
-                text: "DomainKeys Identified Mail, checks if records for DKIM are configured",
-                link: "https://dmarcian.com/dkim/"
+                text: "DomainKeys Identified Mail, checks if records for DKIM are configured. ",
+                link: "https://justinverstijnen.nl/enhance-email-security-with-spf-dkim-dmarc/#dkim"
             },
             "DMARC": {
-                text: "Domain-based Message Authentication, Reporting and Conformance",
-                link: "https://dmarc.org/"
+                text: "Domain-based Message Authentication, Reporting and Conformance. ",
+                link: "https://justinverstijnen.nl/enhance-email-security-with-spf-dkim-dmarc/#dmarc"
             },
             "MTA-STS": {
-                text: "Mail Transfer Agent Strict Transport Security, checks if a policy is configured",
-                link: "https://datatracker.ietf.org/doc/html/rfc8461"
+                text: "Mail Transfer Agent Strict Transport Security, checks if a policy is configured. ",
+                link: "https://justinverstijnen.nl/what-is-mta-sts-and-how-to-protect-your-email-flow/"
             },
             "DNSSEC": {
-                text: "Domain Name System Security Extensions, checks if DNSSEC is enabled",
-                link: "https://www.cloudflare.com/learning/dns/dnssec/"
+                text: "Domain Name System Security Extensions, checks if DNSSEC is enabled. ",
+                link: "https://justinverstijnen.nl/configure-dnssec-and-smtp-dane-with-exchange-online-microsoft-365/"
             }
         };
 
-        // Vul de tabel met de resultaten en tooltips
         for (const [type, record] of Object.entries(data)) {
             if (type === 'NS' || type === 'WHOIS') continue;
 
             const row = document.createElement("tr");
             const typeCell = document.createElement("td");
 
-            // Voeg de tooltip als een extra span toe binnen de td voor het recordtype
             typeCell.innerHTML = `
                 <b class="tooltip">${type}
-                    <span class="tooltip-text" data-tooltip="${type}">${tooltips[type].text} <a href="${tooltips[type].link}" target="_blank">Meer info</a></span>
+                    <span class="tooltip-text" data-tooltip="${type}">${tooltips[type].text} <a href="${tooltips[type].link}" target="_blank">Learn more</a></span>
                 </b>
             `;
 
@@ -79,7 +77,6 @@ async function checkDomain() {
             tbody.appendChild(row);
         }
 
-        // Confetti effect als alle records groen zijn
         let allGreen = true;
         for (const [type, record] of Object.entries(data)) {
             if (type === 'NS' || type === 'WHOIS') continue;
@@ -96,7 +93,6 @@ async function checkDomain() {
             });
         }
 
-        // Extra informatie voor Nameservers en WHOIS
         if (data.NS) {
             const nsBox = document.createElement("div");
             nsBox.className = "infobox";
@@ -124,7 +120,7 @@ async function checkDomain() {
 
     } catch (e) {
         console.error(e);
-        alert("Something went wrong while looking up your domain..");
+        alert("An error occured. My apologies for the inconvenience.");
     } finally {
         loader.style.display = "none";
         resultsSection.style.display = "block";
