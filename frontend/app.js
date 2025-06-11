dca4cb38e1752afd93601b736851530f73861bf2
Justin Verstijnen <info@justinverstijnen.nl>
2025-06-11 20:35:42 +0200

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -30,8 +30,7 @@ async function checkDomain() {
 
             const row = document.createElement("tr");
             const typeCell = document.createElement("td");
-            // We make the entire tooltip a clickable link
-            typeCell.innerHTML = `<a href="${tooltips[type]}" class="tooltip" target="_blank">${type}</a>`;
+            typeCell.innerHTML = `<b class="tooltip" data-tooltip="${tooltips[type]}">${type}</b>`;
             const statusCell = document.createElement("td");
             statusCell.textContent = record.status ? "✅" : "❌";
 
